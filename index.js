ba7737b490d6828b57d898af2ea7a2d04927a93d
Johan Preynat <johan.preynat@gmail.com>
2016-04-28 10:33:05 +0200

    Include scripts using CDN the GitBook assets

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 module.exports = {
     book: {
-        assets: "./book",
+        assets: './book',
         js: [
-            "jquery-2.1.1.min.js",
-            "underscore-min.js",
-            "sequence-diagram-min.js",
-            "plugin.js"
+            'https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.2/raphael-min.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/js-sequence-diagrams/1.0.6/sequence-diagram-min.js',
+            'plugin.js'
         ],
         css: [
-          "styles.css"
-        ],
-        html: {
-            "head:end": function() {
-                // Required to including this way because raphael is non-AMD compliant
-                return "<script src='//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.2/raphael-min.js'></script>";
-            }
-        }
+          'styles.css'
+        ]
     }
 };
