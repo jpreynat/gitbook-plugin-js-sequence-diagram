b54c3aaf38d29867649981ded7de04cff60c510a
Johan Preynat <johan.preynat@gmail.com>
2016-04-28 11:34:57 +0200

    Use configuration theme for sequence-diagram

--- book/plugin.js
+++ book/plugin.js
@@ -1,10 +1,17 @@
-  gitbook.events.bind("page.change", function() {
 require(['gitbook', 'jQuery'], function(gitbook, $) {
+  // Store sequence-diagram theme
+  var userTheme = null;
+
+  gitbook.events.on('start', function(e, config) {
+    userTheme = config['js-sequence-diagram'].theme;
+  });
+
+  gitbook.events.bind('page.change', function() {
     $('code.lang-sequence').each(function(index, element) {
       var $element = $(element);
-      $element.sequenceDiagram({theme: 'simple'});
+      $element.sequenceDiagram({ theme: userTheme });
 
-      var wrapper = $("<div class='scroll'></div>");
+      var wrapper = $('<div class="scroll"></div>');
       wrapper.html($element.html());
       $element.parent().replaceWith(wrapper);
     });
